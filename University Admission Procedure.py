b44c7dcd7f134722ad711b091d647f34e47c3581
Breathe-of-fate <ibbegunov@ya.ru>
2022-09-13 16:45:09 +0300

    University Admission Procedure
    Rethincked and rewrited code for project from Hyperskills (JetBrains Academy)

--- University Admission Procedure.py
+++ University Admission Procedure.py
@@ -1,28 +1,22 @@
+with open("applicants.txt") as open_file:
+    applicants = [i.split() for i in open_file.read().splitlines()]
+
+by_deps = {"Biotech": [], "Chemistry": [], "Engineering": [], "Mathematics": [], "Physics": []}
+ranks_for_deps = {"Biotech": (3, 2), "Chemistry": (3, 3), "Engineering": (5, 4), "Mathematics": (4, 4), "Physics": (2, 4)}
+
 limit = int(input())
 
-applicants = [i.split() for i in open("applicants.txt").read().splitlines()]
+for indicies in range(7, 10):
 
-faculty = ['Biotech', 'Chemistry', 'Engineering', 'Mathematics', 'Physics']
-lessons_rank_indexes = [(3, 2), (3, 3), (5, 4), (4, 4), (2, 4)]
-lessons_rank_dict = {faculty[i]: lessons_rank_indexes[i] for i in range(len(faculty))}
+    for dep, ranks in ranks_for_deps.items():
+        rank1, rank2 = ranks
 
-app_for_deps = {i: [] for i in faculty}
+        for student in sorted(applicants, key=lambda x: (-max((float(x[rank1]) + float(x[rank2])) / 2, float(x[6])), x[0], x[1])): # 1st sort, by mean, exam, name, surname
+            if student[indicies] == dep and len(by_deps[student[indicies]]) < limit:
+                by_deps[student[indicies]].append([student[0], student[1], max((float(student[rank1]) + float(student[rank2])) / 2, float(student[6]))])
+                applicants.remove(student)
 
-for i in range(7, 10):
-    for ii, j in lessons_rank_dict.items():
-        n_1, n_2 = j
-        applicants.sort(key=lambda x: (-max((float(x[n_1]) + float(x[n_2])) / 2, float(x[6])), x[0], x[1]))
-        for iii in applicants.copy():
-            if iii[i] == ii and len(app_for_deps[iii[i]]) < limit:
-                result = iii[0], iii[1],\
-                         max((float(iii[n_1]) + float(iii[n_2])) / 2, float(iii[6]))
-                app_for_deps[iii[i]].append(result)
-                applicants.remove(iii)
-
-for applicants in app_for_deps.values():
-    applicants.sort(key=lambda x: (-float(x[2]), x[0], x[1]))
-
-for department, applicants in app_for_deps.items():
-    with open(department + ".txt", "w") as department_file:
-        for iii in applicants:
-            print(*iii, file=department_file)
+for dep, students in by_deps.items():
+    with open(dep + ".txt", "w") as for_file:
+        for student in sorted(students, key=lambda x: (-float(x[2]), x[0], x[1])): # 2nd sort, by mean, name and surname
+            print(*student, file=for_file)
